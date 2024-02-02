7ffbe735e0f48df16554dfd2b5f2788ca30dbadd
65763 <noahsantan22@gmail.com>
2024-02-02 12:41:31 +0100

    wasd

--- S0180_morris_exercise.py
+++ S0180_morris_exercise.py
@@ -39,6 +39,7 @@ hunger = 0
 whisky = 0
 gold = 0
 
+
 def check_stats():
     global sleepiness, thirst, hunger, whisky, gold
     if sleepiness < 0:
@@ -61,44 +62,111 @@ def check_stats():
 
 """activities"""
 
+
 def sleep():
     global sleepiness, thirst, hunger, whisky, gold
-    sleepiness -= 10; thirst += 1; hunger += 1; whisky += 0; gold += 0
+    if sleepiness > 0:
+        sleepiness -= 10;
+        thirst += 1;
+        hunger += 1;
+        whisky += 0;
+        gold += 0
+    else:
+        print("Not feeling sleepy.")
     check_stats()
     return
+
 
 def mine():
     global sleepiness, thirst, hunger, whisky, gold
-    sleepiness += 5; thirst += 5; hunger += 5; whisky += 0; gold += 5
+    sleepiness += 5;
+    thirst += 5;
+    hunger += 5;
+    whisky += 0;
+    gold += 5
     check_stats()
     return
+
 
 def eat():
     global sleepiness, thirst, hunger, whisky, gold
-    sleepiness += 5; thirst -= 5; hunger -= 20; whisky += 0; gold -= 2
+    sleepiness += 5;
+    thirst -= 5;
+    hunger -= 20;
+    whisky += 0;
+    gold -= 2
     check_stats()
     return
+
 
 def buy_whisky():
     global sleepiness, thirst, hunger, whisky, gold
-    sleepiness += 5; thirst += 1; hunger += 1; whisky += 1; gold -= 1
+    if whisky < 10:
+        sleepiness += 5;
+        thirst += 1;
+        hunger += 1;
+        whisky += 1;
+        gold -= 1
+    else:
+        print("Can't but more than 10 whisky.")
     check_stats()
     return
 
+
 def drink():
     global sleepiness, thirst, hunger, whisky, gold
-    sleepiness += 5; thirst -= 15; hunger -= 1; whisky -= 1; gold += 0
+    if whisky > 0:
+        sleepiness += 5;
+        thirst -= 15;
+        hunger -= 1;
+        whisky -= 1;
+        gold += 0
+    else:
+        print("Not enough drinks.")
     check_stats()
     return
 
 
 """program"""
 
+
 def game_over():
     print("GAME OVER!\nmorris died.")
     exit(0)
+
 
 def game_won():
     print("GAME WON!\nmorris collected all the gold.")
     exit(0)
 
+
+def auto_play():
+    return
+
+
+def manual_play():
+    global gold
+    while True:
+        _input = input("Goal is 1000 gold - sleepiness, thirst or hunger > 100 = game over\n\nactions:"
+                       "\nsleep; -10 sleepiness - +1 thurst - +1 hunger."
+                       "\nmine; +5 sleepiness - +5 thurst - +5 hunger - +5 gold"
+                       "\neat; +5 sleepiness - -5 thurst - +20 hunger - -2 gold"
+                       "\nbuy whisky; +5 sleepiness - +1 thurst - +1 hunger - +1 whisky - -1 gold"
+                       "\ndrink; +5 sleepiness - -15 thurst - -1 whisky"
+                       f"\n\nstats: sleepiness: {sleepiness} - thirst: {thirst} - hunger: {hunger} - whisky: {whisky} - gold: {gold}\n")
+
+        if _input == "1":
+            sleep()
+        if _input == "2":
+            mine()
+        if _input == "3":
+            eat()
+        if _input == "4":
+            buy_whisky()
+        if _input == "5":
+            drink()
+        if gold >= 1000:
+            game_won()
+
+
+manual_play()
