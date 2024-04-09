5e8b2ecc104c7e0c2eb2ed562d0d9227cd4bd049
65763 <noahsantan22@gmail.com>
2024-04-09 14:45:24 +0200

    school laptop

--- S0100_test.py
+++ S0100_test.py
@@ -1,5 +1,20 @@
-for i in range(9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999):
-    print(i)
+import time
+import ctypes
 
-    if i%3 == 0 or i%5 == 0:
-        NotImplemented
+kernel32 = ctypes.WinDLL('Kernel32')
+handle = kernel32.GetStdHandle(-11)
+
+def move_cursor(x, y):
+    coordinate = ctypes.c_long(y << 16 | x)
+    kernel32.SetConsoleCursorPosition(handle, coordinate)
+
+def countdown():
+    for i in range(10):
+        print(f"Exiting in {10 - i} ")
+        time.sleep(1)
+        move_cursor(0, 0)
+    print("Exiting")
+    time.sleep(1)
+
+
+countdown()
