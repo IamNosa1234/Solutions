1275a53cf96705518f94b88edb4adb5bf3407abd
65763 <noahsantan22@gmail.com>
2024-11-12 14:41:09 +0100

    school laptop

---
+++ S8010.py
@@ -0,0 +1,64 @@
+# Idea: create datasets from my discord community
+# create funny statistics and graphs using this data
+
+import os
+from dotenv import load_dotenv
+import pandas as pd
+import matplotlib.pyplot as plt # for plotting
+from discord.ext import commands
+from discord import Intents
+
+# Create a Discord class for discord bot, to connect to the server.
+class DiscordBot(commands.Bot):
+    def __init__(self):
+
+        intents = self.intents()
+
+        self.columns = ["user_id", "channel_id", "message_id", "message_content", "message_lenght", "time_date",
+                        "edited", "last_edit_time_date", "edit_count", "author_name", "author_discriminator", "author_nick",
+                        "channel_name", "is_reply", "reply_to_message_id", "reply_to_message_content"]
+
+        super().__init__(command_prefix="!", intents=intents)
+
+        try:
+            self.data = pd.read_csv("discord_data.csv")
+        except FileNotFoundError as e:
+            print(f"Data file not found. Please make sure to run the data collection script first. ({e})")
+
+        load_dotenv()
+        self.token = os.getenv("DISCORD_BOT_TOKEN")
+
+    @staticmethod
+    def intents():
+        intents = Intents.default()
+        intents.message_content = True
+        intents.messages = True
+        return intents
+
+    async def on_ready(self):
+        print(f"Logged in as {self.user}")
+
+    async def on_connect(self):
+        print(f"Connected to Discord")
+
+    async def on_message(self, message):
+        if message.author == self.user or message.author.bot:
+            return
+        print(f"{message.author}: {message.content}")
+
+        if message.content == "!stats":
+            await message.channel.send("Creating statistics...")
+            self.create_statistics()
+
+    def create_statistics(self):
+        # Create a bar chart for the number of messages sent by each user
+        message_counts = self.data["author"].value_counts()
+        message = "```"
+
+        for author, count in message_counts.items():
+            message += f"{author}: {count}\n"
+
+
+if __name__ == "__main__":
+    bot = DiscordBot()
+    bot.run(bot.token)
